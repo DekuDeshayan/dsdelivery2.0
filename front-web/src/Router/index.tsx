fa993c0b4eb1a5137907b371b4ad128904278a22
DekuDeshayan <buraimoRalph129@gmail.com>
2022-08-16 14:34:44 +0200

    Added Home

--- front-web/src/Router/index.tsx
+++ front-web/src/Router/index.tsx
@@ -1,10 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../Home";
 import Navbar from "../Navbar";
 
 function AppRoutes(){
     return(
         <BrowserRouter>
             <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+            </Routes>
         </BrowserRouter>
     )
 }
