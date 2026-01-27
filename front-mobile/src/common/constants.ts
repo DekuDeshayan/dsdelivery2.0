bdeb16bc6dbb849bac73140402976eaa389972d3
Ralph Buraimo <r.buraimo@rio-kobe.co.jp>
2026-01-27 18:03:41 +0900

    Backend Integration & google maps navigation Integration

--- front-mobile/src/common/constants.ts
+++ front-mobile/src/common/constants.ts
@@ -1,3 +1,1 @@
-// export const BASE_API_URL = 'http://localhost:8080/api';
-export const BASE_API_URL = 'http://192.168.179.32:8080/api';
-export const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoicmJ1cmFpbW8iLCJhIjoiY21rcXBveHZwMHV1NzNjcTRtbW93dTUyZyJ9.fZX2RqmfcKp1KTjJnv-CSw";
+export const BASE_API_URL = 'http://localhost:8080/api';
